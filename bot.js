eadda454030d3f16d50e752fa8232643c687fac4
abood142 <44087788+abood142@users.noreply.github.com>
2018-10-15 19:10:46 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,44 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+client.on('ready', () => {
+    client.user.setActivity("$bc",{type: 'PLAYING'});
+});
+client.on('ready', () => {
+    client.user.setStatus(" online");
+ 
+ });
+ 
+ 
+ client.on('message', message => {
+if (message.content.split(' ')[0] == '$bc')
+ message.guild.members.forEach( member => {
+         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+member.send( `${member} ! ` + "**" + " : ** " + message.content.substr(3));
+                                                            message.delete();
+});
+});
+
+
+client.on("message", message => {
+    var prefix = "$";
+ 
+            var args = message.content.substring(prefix.length).split(" ");
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+                            let embed4 = new Discord.RichEmbed()
+             .setDescription("**:white_check_mark: | جاري ارسال البرودكاست**")
+           .addField("مرسل البرودكاست" , message.author)
+          .addField("نص البرودكاست" ,args.join("  "))
+                            .addField("عدد الاعضاء المرسل لهم :busts_in_silhouette:" ,` **[${message.guild.memberCount}]**`,true)
+                                                            .setColor("#008000")
+                                message.channel.sendEmbed(embed4);
+                                                      message.delete();
+                            
+                          }
+});
+
+client.login(process.env.BOT_TOKEN);
